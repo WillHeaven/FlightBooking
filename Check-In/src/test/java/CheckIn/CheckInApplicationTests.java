909ff34c991d930b2f27f2fa512616fc8bd34de3
ACER <ACER@LAPTOP-38LL9KTP>
2022-09-26 18:16:52 +0530

    ControllerTest

--- Check-In/src/test/java/CheckIn/CheckInApplicationTests.java
+++ Check-In/src/test/java/CheckIn/CheckInApplicationTests.java
@@ -2,10 +2,13 @@ package CheckIn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class CheckInApplicationTests {
 
+	
 	@Test
 	void contextLoads() {
 	}
--- Check-In/src/test/java/CheckIn/ControllerMockitoTest.java
+++ Check-In/src/test/java/CheckIn/ControllerMockitoTest.java
@@ -1,10 +1,10 @@
 package CheckIn;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +13,14 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import CheckIn.Controller.CheckinController;
 import CheckIn.Model.Booking;
@@ -26,6 +29,7 @@ import CheckIn.Model.Flight;
 import CheckIn.Model.FlightData;
 import CheckIn.Model.Passenger;
 import CheckIn.Service.CheckInService;
+import CheckIn.rabbitmq.MessagingConfiq;
 
 @SpringBootTest(classes= {ControllerMockitoTest.class})
 public class ControllerMockitoTest {
@@ -36,16 +40,23 @@ public class ControllerMockitoTest {
 	@InjectMocks
 	CheckinController checkincon;
 	
-@LocalServerPort
-int randomServerPort;
+//	@Autowired
+//	 RabbitTemplate template;  
 
-@Autowired
-private TestRestTemplate restTemplate;
-
-	@Test
+//	@Autowired
+//	RestTemplate restTemplate;
+	
+	
+//	@Test
+//	@Order(1)
+//	public 
+	
+/*	@Test
 	@Order(1)
-	public void test_getInfo() throws URISyntaxException
+	public void test_getInfo() 
 	{
+		long i=1234;
+		LocalDate l=LocalDate.now();
 		List<FlightData> d=new  ArrayList<FlightData>();
 		d.add(new FlightData("1234","3423","12:50","9:20") );
 		
@@ -57,23 +68,19 @@ private TestRestTemplate restTemplate;
 		p.add(new Passenger(1,"Nirmal",3,5,334));
 		p.add(new Passenger(2,"Sintha",4,6,335));
 		
+		Booking b=new Booking(i,f,l,p,20000.00,false,false,false);
+		
 		CheckIn c= new CheckIn(1234,f,p,"Yes",2345);
-		long i=1234;
-	     
-	 final String baseUrl = "http://localhost:" + randomServerPort + "/booking/"+i;
-		    URI uri = new URI(baseUrl);
-		    
-		    //final String baseUrl = "http://localhost:8081/booking/"+id;
 	
-		  Booking b=this.restTemplate.getForObject(baseUrl,Booking.class);
-		  Assertions.assertEquals(true,b.getBooking_id()==i);
-		  
+		RestTemplate restTemplate= new RestTemplate();
+		when(restTemplate.getForObject("http://localhost:8081/booking/"+i,Booking.class)).thenReturn(b);
+		assertEquals(i,b.getBooking_id());
 		  
 		when(checkincon.getInfo(i)).thenReturn(c);
 		CheckIn ch=checkincon.getInfo(i);
 		
 		assertEquals(i,ch.getBookingId());
-	}
+	} */
 	
 /*	@Test
 	@Order(2)
@@ -92,11 +99,39 @@ private TestRestTemplate restTemplate;
 		
 		CheckIn c= new CheckIn(1234,f,p,"Yes",2345);
 		
+		when(template.convertAndSend(MessagingConfiq.EXCHANGE,MessagingConfiq.ROUTING_KEY,checkinstatus)).thenReturn()
+		
 		when(checkinser.confirmation(c)).thenReturn(c);
 		String s=checkincon.confirm();
 		
 		assertEquals("Check-In has confirmed"+c.getCheckno(),s);
 		
-	}*/
+	}
+	*/
 	
+	@Test
+	@Order(1)
+	public void test_Update()
+	{
+		long pnr=1234;
+		LocalDate l=LocalDate.now();
+		List<FlightData> d=new  ArrayList<FlightData>();
+		d.add(new FlightData("1234","3423","12:50","9:20") );
+		
+		
+		List<Flight> f=new ArrayList<Flight>();
+		f.add(new Flight("1234","Chennai","Mumbai",6000,60,30,"9:20",d));
+		
+		List<Passenger> p=new ArrayList<Passenger>();
+		p.add(new Passenger(1,"Nirmal",3,5,334));
+		p.add(new Passenger(2,"Sintha",4,6,335));
+		
+		Booking b=new Booking(pnr,f,l,p,20000.00,false,true,false);
+		Booking c=new Booking(pnr,f,l,p,20000.00,false,false,false);
+		c.setChecked_in(true);
+		RestTemplate restTemplate= new RestTemplate();
+		
+		//Mockito.when(restTemplate.put("http://localhost:8081/booking/booking/"+ pnr,c)).println("");
+		assertEquals("Checked In Successfully:"+pnr,"Checked In Successfully:"+b.booking_id);
+	}
 }
